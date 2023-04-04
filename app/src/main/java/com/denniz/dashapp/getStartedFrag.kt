d39c3f90de7f214abd0ce30dd12a3acbd3e533a6
dator <dennizyilmaz1903@gmail.com>
2023-04-05 00:04:15 +0200

    i added some false handling. When the user haven't typed in any credentials the fields turn red and get's a toast message saying No credentials.

--- app/src/main/java/com/denniz/dashapp/getStartedFrag.kt
+++ app/src/main/java/com/denniz/dashapp/getStartedFrag.kt
@@ -1,6 +1,9 @@
 package com.denniz.dashapp
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.fragment.app.Fragment
@@ -11,8 +14,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -20,7 +25,6 @@ class getStartedFrag : Fragment() {
 
 
     private lateinit var auth: FirebaseAuth
-
     @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +36,9 @@ class getStartedFrag : Fragment() {
         val editTextEmail = view.findViewById<EditText>(R.id.editTextEmail)
         val editTextPassword = view.findViewById<EditText>(R.id.editTextPassword)
         val editTextConfirmPassword = view.findViewById<EditText>(R.id.editTextConfirmPassword)
+        val textInputLayoutEmail = view.findViewById<TextInputLayout>(R.id.textInputLayoutEmail)
+        val textInputLayoutPassword = view.findViewById<TextInputLayout>(R.id.textInputLayoutPassword)
+        val textInputLayoutConfirmPassword = view.findViewById<TextInputLayout>(R.id.textInputLayoutConfirmPassword)
         auth = FirebaseAuth.getInstance()
 
         signUpBtn.setOnClickListener() {
@@ -49,6 +56,25 @@ class getStartedFrag : Fragment() {
                         }
                     }
                 }
+            }
+            if (email.isEmpty() && password.isEmpty() && confirmPassword.isEmpty()){
+                val text = "No credentials. Please try again."
+                val colorStateList = ColorStateList(
+                    arrayOf(
+                        intArrayOf(android.R.attr.state_focused),
+                        intArrayOf(-android.R.attr.state_focused)
+                    ),
+                    intArrayOf(
+                        ContextCompat.getColor(requireContext(), R.color.colorError),
+                        ContextCompat.getColor(requireContext(), R.color.colorError)
+                    ))
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+                textInputLayoutEmail.defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.colorError))
+                textInputLayoutEmail.setBoxStrokeColorStateList(colorStateList)
+                textInputLayoutPassword.defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.colorError))
+                textInputLayoutPassword.setBoxStrokeColorStateList(colorStateList)
+                textInputLayoutConfirmPassword.defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.colorError))
+                textInputLayoutConfirmPassword.setBoxStrokeColorStateList(colorStateList)
             }
         }
 
